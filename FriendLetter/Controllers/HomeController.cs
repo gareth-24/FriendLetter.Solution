60c75d98f6d92d29d926ff0e4e94b433f95a8a54
Gareth Grindeland <gt.grindeland@gmail.com>
2023-05-08 10:28:43 -0700

    add homecontroller and letter.cshtml file

--- FriendLetter/Controllers/HomeController.cs
+++ FriendLetter/Controllers/HomeController.cs
@@ -11,6 +11,24 @@ namespace FriendLetter.Controllers
     public string Goodbye() { return "Goodbye friend."; }
 
     [Route("/")]
-    public ActionResult Letter() { return View(); }
+    public ActionResult Letter()
+    {
+      LetterVariable myLetterVariable = new LetterVariable();
+      myLetterVariable.Recipient = "Lina";
+      myLetterVariable.Sender = "Jasmine";
+      return View(myLetterVariable);
+    }
+
+    [Route("/form")]
+    public ActionResult Form() { return View(); }
+
+    [Route("/postcard")]
+    public ActionResult Postcard(string recipient, string sender)
+    {
+      LetterVariable myLetterVariable = new LetterVariable();
+      myLetterVariable.Recipient = recipient;
+      myLetterVariable.Sender = sender;
+      return View(myLetterVariable);
+    }
   }
 }
